cbc3934d359606a4758ba05c44efb6a1700e174b
groteck <groteck@gmail.com>
2013-05-25 00:47:07 +0100

    add corp rack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :cors_set_access_control_headers
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
