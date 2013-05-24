76595f8e63dec790b0e1a1b52e7f18eb05dd6881
groteck <groteck@gmail.com>
2013-05-24 23:55:59 +0100

    add after filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   after_filter :cors_set_access_control_headers
+  before_filter :cors_preflight_check
 
   # For all responses in this controller, return the CORS access control headers.
 
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'http://localhost:4567'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
     headers['Access-Control-Max-Age'] = "1728000"
   end
@@ -16,7 +17,7 @@ class ApplicationController < ActionController::Base
 
   def cors_preflight_check
     if request.method == :options
-      headers['Access-Control-Allow-Origin'] = 'http://localhost:4567'
+      headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
       headers['Access-Control-Max-Age'] = '1728000'
