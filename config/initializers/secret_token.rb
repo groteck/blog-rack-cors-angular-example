a59b2b10765365971909c87dd4742a1b9ffd9e16
groteck <groteck@gmail.com>
2013-05-24 22:23:54 +0100

    my firest commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogTest::Application.config.secret_token = 'cda015dd02b9d12b6894bb13a2f682825a463abe7347d8590075ee0f10902a07c8736dd69f7c9ff70e0fbe44b28847c2761092b128bcab4aef718132d5cafd05'
